eb9014748083a430f2d43fda00b488c047f2db8d
helminafan <nafan2332@gmail.com>
2022-10-06 22:45:27 +0800

    tes

---
+++ database/migrations/2022_10_06_143813_create_nasabah_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nasabah', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 50);
+            $table->string('no_hp', 12);
+            $table->string('foto', 50);
+            $table->date('tgl_join');
+            $table->date('tgl_lahir');
+            $table->unsignedBigInteger('iddawis');
+            $table->foreign('iddawis')->references('id')->on('dawis')->constrained()->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nasabah');
+    }
+};
