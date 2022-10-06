132d95daa3fbe6cf92575a401677262191a66512
helminafan <nafan2332@gmail.com>
2022-10-06 22:25:35 +0800

    tes

--- database/migrations/2022_10_06_131213_create_dawis_table.php
+++ database/migrations/2022_10_06_131213_create_dawis_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('dawis', function (Blueprint $table) {
             $table->id();
             $table->string('nama', 50);
-            $table->timestamps();
         });
     }
 
