7e1e13acbedced6e8f6de30b225bdd6013a7b69c
helminafan <nafan2332@gmail.com>
2022-10-07 22:06:54 +0800

    tes

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -3,7 +3,7 @@
      <!-- Sidebar - Brand -->
      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
          <div class="sidebar-brand-icon">
-             <img src="{{asset('backend/img/logo2.png')}}" style="height: 40px; width: 40px;"  alt="">
+             <img src="{{asset('backend/img/logo2.png')}}" style="height: 40px; width: 40px;" alt="">
          </div>
          <div class="sidebar-brand-text mx-2">Bank Sampah</div>
      </a>
@@ -23,39 +23,37 @@
 
      <!-- Heading -->
      <div class="sidebar-heading">
-         Interface
+         Pengguna
      </div>
 
      <!-- Nav Item - Pages Collapse Menu -->
      <li class="nav-item">
          <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-             <i class="fas fa-fw fa-cog"></i>
-             <span>Components</span>
+             <i class="fa-solid fa-users"></i>
+             <span>Penguna</span>
          </a>
          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
              <div class="bg-white py-2 collapse-inner rounded">
-                 <h6 class="collapse-header">Custom Components:</h6>
-                 <a class="collapse-item" href="buttons.html">Buttons</a>
-                 <a class="collapse-item" href="cards.html">Cards</a>
+                 <h6 class="collapse-header">Jenis Pengguna:</h6>
+                 <a class="collapse-item" href="buttons.html">Nasabah</a>
+                 <a class="collapse-item" href="cards.html">Dawis</a>
+                 <a class="collapse-item" href="cards.html">Petugas</a>
              </div>
          </div>
      </li>
 
      <!-- Nav Item - Utilities Collapse Menu -->
+     <hr class="sidebar-divider">
+     <div class="sidebar-heading">
+         Tabungan
+     </div>
      <li class="nav-item">
-         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-             <i class="fas fa-fw fa-wrench"></i>
-             <span>Utilities</span>
+
+         <a class="nav-link collapsed" href="#"  data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+             <i class="fa-solid fa-piggy-bank"></i>
+             <span>Tabungan</span>
          </a>
-         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-             <div class="bg-white py-2 collapse-inner rounded">
-                 <h6 class="collapse-header">Custom Utilities:</h6>
-                 <a class="collapse-item" href="utilities-color.html">Colors</a>
-                 <a class="collapse-item" href="utilities-border.html">Borders</a>
-                 <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                 <a class="collapse-item" href="utilities-other.html">Other</a>
-             </div>
-         </div>
+         
      </li>
 
      <!-- Divider -->
@@ -63,42 +61,27 @@
 
      <!-- Heading -->
      <div class="sidebar-heading">
-         Addons
+         Transaksi
      </div>
 
      <!-- Nav Item - Pages Collapse Menu -->
      <li class="nav-item">
          <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-             <i class="fas fa-fw fa-folder"></i>
-             <span>Pages</span>
+             <i class='bx bxs-wallet'></i>
+             <span>Transaksi</span>
          </a>
          <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
              <div class="bg-white py-2 collapse-inner rounded">
-                 <h6 class="collapse-header">Login Screens:</h6>
-                 <a class="collapse-item" href="login.html">Login</a>
-                 <a class="collapse-item" href="register.html">Register</a>
-                 <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                 <div class="collapse-divider"></div>
-                 <h6 class="collapse-header">Other Pages:</h6>
-                 <a class="collapse-item" href="404.html">404 Page</a>
-                 <a class="collapse-item" href="blank.html">Blank Page</a>
+                 <h6 class="collapse-header">Transaksi : </h6>
+                 <a class="collapse-item" href="login.html">Tagihan</a>
+                 <a class="collapse-item" href="register.html">Detail Tagihan</a>
+
              </div>
          </div>
      </li>
 
-     <!-- Nav Item - Charts -->
-     <li class="nav-item">
-         <a class="nav-link" href="charts.html">
-             <i class="fas fa-fw fa-chart-area"></i>
-             <span>Charts</span></a>
-     </li>
 
-     <!-- Nav Item - Tables -->
-     <li class="nav-item">
-         <a class="nav-link" href="tables.html">
-             <i class="fas fa-fw fa-table"></i>
-             <span>Tables</span></a>
-     </li>
+
 
      <!-- Divider -->
      <hr class="sidebar-divider d-none d-md-block">
