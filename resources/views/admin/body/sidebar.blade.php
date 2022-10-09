76be17e2369925a04b2268eaa48a3c01a83bce8a
ajil23 <azrilpraya@gmail.com>
2022-10-09 09:09:59 +0800

    ajril

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -73,9 +73,8 @@
          <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
              <div class="bg-white py-2 collapse-inner rounded">
                  <h6 class="collapse-header">Transaksi : </h6>
-                 <a class="collapse-item" href="login.html">Tagihan</a>
-                 <a class="collapse-item" href="register.html">Detail Tagihan</a>
-
+                 <a class="collapse-item" href="{{route('user.tagihan')}}">Tagihan</a>
+                 <a class="collapse-item" href="{{route('user.detagihan')}}">Detail Tagihan</a>
              </div>
          </div>
      </li>
