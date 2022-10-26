c95e477a8394382fec078f560ec746fa111a7e49
samCZ054 <aramadhan39@gmail.com>
2022-10-26 17:20:28 +0700

    User role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['Petugas', 'Admin', 'Nasabah']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['Petugas', 'Admin', 'Nasabah']);
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
