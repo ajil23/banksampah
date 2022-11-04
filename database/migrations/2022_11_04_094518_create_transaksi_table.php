25a08771e1e65d28605b85a2e69cdf772962dd5b
helminafan <nafan2332@gmail.com>
2022-11-04 18:02:58 +0800

    tes

---
+++ database/migrations/2022_11_04_094518_create_transaksi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('nasabah_id');
+            $table->foreign('nasabah_id')->references('id')->on('nasabah')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->text('keterangan_pembelian');
+            $table->integer('nominal');
+            $table->timestamp('tgl_transaksi')->useCurrent();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
