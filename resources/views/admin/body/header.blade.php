ee50eababe86b92812faad0fac3432826cbf2e44
ajil23 <azrilpraya@gmail.com>
2022-12-06 23:31:28 +0800

    fixing header

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -159,7 +159,7 @@
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
+                {{-- <a class="dropdown-item" href="#">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
                 </a>
@@ -170,8 +170,7 @@
                 <a class="dropdown-item" href="#">
                     <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                     Activity Log
-                </a>
-                <div class="dropdown-divider"></div>
+                </a> --}}
                 <a class="dropdown-item" href="{{route('admin.logout')}}">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
